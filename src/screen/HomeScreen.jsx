4988ea300727f3eadfcb65479faf9d1cd0008ab6
unknown <sandeepmauryaworld>
2024-12-10 20:59:33 +0530

    till redux

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import products from '../Product'
 import { Col, Row } from 'react-bootstrap'
 import ProductScreen from './ProductScreen'
+import axios from 'axios'
 
 
 
 const HomeScreen = () => {
+  const [productList,setProductList]=useState([])
+
+
+
+  useEffect(()=>{
+    const fetchProducts=async()=>{
+
+      const {data}=await axios.get('/api/products')
+      setProductList(data)
+    } 
+
+    fetchProducts()
+  },[])
+
+  console.log(productList,'qqqqqqq');
+  
+  
   return (
     <Row>
         {products.map((product,index)=>{
--- src/screen/ProductDetails.jsx
+++ src/screen/ProductDetails.jsx
@@ -1,49 +1,62 @@
-import React from "react";
-import products from "../Product";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Row, Col, ListGroup, Button, Image, ListGroupItem } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import Ratings from "../components/Ratings";
-const ProductDetails = ({ match }) => {
-    const {id}=useParams()
-  const Product = products.find((p) => p._id ===id);
+
+const ProductDetails = () => {
+  const { id } = useParams(); // Extract the product ID from the route
+  const [product, setProduct] = useState({}); // Initialize product as an object
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [id]); // Add `id` as a dependency
+
   return (
     <div>
-      <Link to='/'>GO Back</Link>
+      <Link to="/">GO Back</Link>
       <Row>
         <Col md={6}>
-          <Image src={Product.image} alt={Product.image} fluid />
+          <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={3}>
-        <ListGroup variant="flush">
+          <ListGroup variant="flush">
             <ListGroupItem>
-                <h3>{Product.name}</h3>
+              <h3>{product.name}</h3>
             </ListGroupItem>
             <ListGroupItem>
-                <Ratings value={Product.rating} text={`${Product.numReviews}Reviews`}/>
+              <Ratings value={product.rating} text={`${product.numReviews} Reviews`} />
             </ListGroupItem>
             <ListGroupItem>
-              Price:-{Product.price}
+              Price: {product.price}
             </ListGroupItem>
             <ListGroupItem>
-              Description:-{Product.description}
+              Description: {product.description}
             </ListGroupItem>
-        </ListGroup>
+          </ListGroup>
         </Col>
         <Col md={3}>
-        <ListGroupItem>
-          <Row>
-            <Col>
-            Status :
-            </Col>
-            <Col>
-            {Product.countInStock=== 0 ? "Product Out Of Stock":'Product In Stock'}
-            </Col>
-          </Row>
-
-        </ListGroupItem>
-        <ListGroupItem>
-          <Button className="btn-block " type="button">Add to Cart</Button>
-        </ListGroupItem>
+          <ListGroup>
+            <ListGroupItem>
+              <Row>
+                <Col>Status:</Col>
+                <Col>{product.countInStock === 0 ? "Out Of Stock" : "In Stock"}</Col>
+              </Row>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Button
+                className="btn-block"
+                type="button"
+                disabled={product.countInStock === 0}
+              >
+                Add to Cart
+              </Button>
+            </ListGroupItem>
+          </ListGroup>
         </Col>
       </Row>
     </div>
